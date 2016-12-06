d4ce0b7135bea0b81ef859aea826b9efb0d1496b
Andrei Stoica <andrei.stoica@uoit.net>
2016-12-06 13:58:59 -0500

    added activities for each function

--- PokedexUI/app/src/main/java/com/example/brad/pokedexui/Pokedex.kt
+++ PokedexUI/app/src/main/java/com/example/brad/pokedexui/Pokedex.kt
@@ -1,13 +1,33 @@
 package com.example.brad.pokedexui
 
+import android.R.attr.button
+import android.R.attr.onClick
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.widget.Button
+import com.example.brad.pokedexui.R.layout.search_page
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.onClick
 
 class Pokedex : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
         setContentView(R.layout.activity_pokedex)
-        setContentView(R.layout.create_team_page)
+
+        val search = findViewById(R.id.searchButton) as Button
+        val favButton = findViewById(R.id.favoritesButton) as Button
+        val createButoon = findViewById(R.id.createButton) as Button
+
+
+
+        search.onClick { startActivity(intentFor<Search>()) }
+        favButton.onClick { startActivity(intentFor<Favorites>())}
+        createButoon.onClick { startActivity(intentFor<CreateTeam>()) }
     }
+
 }
