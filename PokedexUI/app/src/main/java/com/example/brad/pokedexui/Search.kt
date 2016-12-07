13582e436108fb40b026a4fd2c9f1441f1cf5269
Andrei Stoica <andrei.stoica@uoit.net>
2016-12-07 17:57:12 -0500

    Implemented search

--- PokedexUI/app/src/main/java/com/example/brad/pokedexui/Search.kt
+++ PokedexUI/app/src/main/java/com/example/brad/pokedexui/Search.kt
@@ -1,29 +1,94 @@
 package com.example.brad.pokedexui
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.SearchView
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient
-import me.sargunvohra.lib.pokekotlin.model.NamedApiResourceList
 import org.jetbrains.anko.*
-import java.util.concurrent.Future
+import kotlin.collections.filter
 
 class Search : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_page)
-        val pokeApi = PokeApiClient()
-        val list = pokeApi.getPokemonList(0,721)
-
+        var mAdapter = MyAdapter(this);
         val searchbox = findViewById(R.id.searchView) as SearchView
+
+        searchbox.queryHint = "search for pokemon "
+
+        searchbox.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(p0: String?): Boolean {
+                mAdapter.search = mAdapter.list.filter { x -> x.name.contains(p0.toString()) }
+                if (p0.isNullOrBlank()){
+                    mAdapter.search = mAdapter.list
+                }
+                return false
+            }
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                mAdapter.search = mAdapter.list.filter { x -> x.name.contains(p0.toString()) }
+                if (p0.isNullOrBlank()){
+                    mAdapter.search = mAdapter.list
+                }
+                return false
+            }
+        })
+
+
         val serchResult = findViewById(R.id.SearchResults) as ListView
-
-
-
-        searchbox.queryHint = "search for pokemon"
-
+        serchResult.adapter = mAdapter
 
     }
+
+    fun getData() : List<PokemonRef> {
+        val pokeApi = PokeApiClient()
+        return pokeApi.getPokemonList(0,721).results.map { p -> PokemonRef(p.name,p.id) }
+    }
+
+
 }
+
+
+
+class MyAdapter(val activity : Search) : BaseAdapter() {
+    var list : List<PokemonRef> = activity.getData()
+    var search: List<PokemonRef> = list
+
+    override fun getView(i : Int, v : View?, parent : ViewGroup?) : View {
+        val item = getItem(i)
+        return with(parent!!.context) {
+            relativeLayout {
+                textView(item.name) {
+                    textSize = 32f
+                }
+                textView(item.id.toString()) {
+                    textSize = 16f
+                }.lparams {
+                    alignParentBottom()
+                    alignParentRight()
+                }
+            }
+        }
+    }
+
+    override fun getItem(position : Int) : PokemonRef {
+        return search.get(position)
+    }
+
+    override fun getCount() : Int {
+        return search.size
+    }
+
+    override fun getItemId(position : Int) : Long {
+        return getItem(position).id.toLong() as Long;
+    }
+
+}
+
+data class PokemonRef (val name: String, val id: Int)
