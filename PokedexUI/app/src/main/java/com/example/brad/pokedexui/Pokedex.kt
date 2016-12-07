7bbcd37b155606054ce23d983ab9329017acbe53
NoobyDahGreat <bradgardiner21@gmail.com>
2016-12-07 11:50:45 -0500

    Some UI changes

--- PokedexUI/app/src/main/java/com/example/brad/pokedexui/Pokedex.kt
+++ PokedexUI/app/src/main/java/com/example/brad/pokedexui/Pokedex.kt
@@ -2,12 +2,22 @@ package com.example.brad.pokedexui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pokedex.*
 
 class Pokedex : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokedex)
-        setContentView(R.layout.create_team_page)
+        button.setOnClickListener {
+
+
+                setContentView(R.layout.activity_pokedex)
+
+        }
     }
+
+
+
+
 }
